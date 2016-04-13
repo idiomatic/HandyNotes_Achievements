a55046ed54d239a8ad2fd2aeeed6f0276cd1a597
brian harrison <brian@idiomatic.org>
2016-04-13 11:34:27 -0700

    more (mostly blank) locales

---
+++ Locale/itIT.lua
@@ -0,0 +1,8 @@
+-- Copyright 2016, r. brian harrison.  all rights reserved.
+
+local ADDON_NAME = ...
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+assert(AceLocale, string.format("%s requires %s", ADDON_NAME, "AceLocale-3.0"))
+
+local L = AceLocale:NewLocale(ADDON_NAME, "itIT", true)
+if not L then return end
