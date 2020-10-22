40aacee65ca6effc15ee047f6614eea2929bbb4c
Brian Harrison <git@rbrian.com>
2020-10-21 19:56:06 -0700

    break off AchievementLocations

--- Libs/AchievementLocations-1.0/AchievementLocations_seasonal.lua
+++
@@ -1,7 +0,0 @@
--- this file is machine generated.  do not edit.
-
-local AL = LibStub:GetLibrary("AchievementLocations-1.0")
-local function A(row) AL:AddLocation(row) end
-
--- World Events: The Captain's Booty
-A{"TheCapeOfStranglethorn", 3457, 0.400, 0.726, season="Pirates' Day"}
--- Libs/AchievementLocations-1.0/AchievementLocations_seasonal_pilgrim.lua
+++
@@ -1,7 +0,0 @@
--- this file is machine generated.  do not edit.
-
-local AL = LibStub:GetLibrary("AchievementLocations-1.0")
-local function A(row) AL:AddLocation(row) end
-
--- World Events: Pilgrim
-A{"SethekkHalls", 3478, 0.326, 0.276, criterion=11272, floor=2, season="Pilgrim's Bounty"} -- Terokkar Turkey Time
